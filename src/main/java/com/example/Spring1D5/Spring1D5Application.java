edda5720e1fcd8e1ef5ed9452aa12135efe6d7ae
ariannaal <ariannaloreti25@gmail.com>
2024-09-06 09:18:40 +0200

    Initial commit

---
+++ src/main/java/com/example/Spring1D5/Spring1D5Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring1D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring1D5/Spring1D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring1D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
